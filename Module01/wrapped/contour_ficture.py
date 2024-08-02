b46f785ed734f4504c935f3cc99ee759aede8f74
daimu <1663457357@qq.com>
2024-08-02 15:04:08 +0800

    分布图

---
+++ Module01/wrapped/contour_ficture.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  2 09:22:08 2024
+
+@author: EDY
+
+分布图： 年平均气温、变率、最大值、最小值、距平、距平百分率、气候值、与上一年比较值、近10年均值、与近10年比较值
+"""
+
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+from Utils.station_to_grid import station_to_grid 
+import os
+from netCDF4 import Dataset
+import netCDF4 as nc
+
+def contour_picture(result_df,data_df,shp_name,method,output_filepath):
+    #%% 数据选取
+    
+    # 插值范围、掩膜
+    gdf = gpd.read_file(shp_name)
+    points = []
+    for polygon in gdf.geometry:
+        if polygon.geom_type == 'Polygon':
+            exterior = polygon.exterior
+            for point in exterior.coords:
+                points.append(point)
+        elif polygon.geom_type == 'MultiPolygon':
+            for part in polygon:
+                exterior = part.exterior
+                for point in exterior.coords:
+                    points.append(point)
+    df_shp = pd.DataFrame(points, columns=['Longitude', 'Latitude'])
+    
+    # 站点经纬度匹配
+    df_sta_1=result_df.T.reset_index()
+    
+    df_sta_1.columns =df_sta_1.iloc[0]
+    df_sta_1 = df_sta_1.drop(df_sta_1.index[0])
+    df_sta_1 = df_sta_1.iloc[:-5:,:]
+    
+    station_id=np.array(df_sta_1.iloc[:,1])
+    
+    df_sta=pd.DataFrame(columns=['Station_id','lon','lat'])
+    
+    df_sta['Station_id']=station_id
+    for i in np.arange(len(station_id)):
+        df_sta.loc[i,'lon']=float(data_df[data_df['Station_Id_C']==station_id[i]]['Lon'].iloc[0])
+        df_sta.loc[i,'lat']=float(data_df[data_df['Station_Id_C']==station_id[i]]['Lat'].iloc[0])
+    
+    # 经纬度选取
+    lon_sta=df_sta['lon'].values
+    lat_sta=df_sta['lat'].values
+    
+    #%% 插值
+    # 网格参数设置
+    start_lon = df_shp['Longitude'].min()-1
+    start_lat = df_shp['Latitude'].min()-1
+    end_lon = df_shp['Longitude'].max()+1
+    end_lat = df_shp['Latitude'].max()+1
+    resolution = 0.01
+        
+    gridx=np.arange(start_lon,end_lon,resolution)
+    gridy=np.arange(start_lat,end_lat,resolution)
+    
+    result=dict()
+    
+    
+    # 历年平均值
+    df_sta_2 = df_sta_1.iloc[:,:-10:].T
+    df_sta_2.columns =df_sta_2.iloc[0]
+    df_sta_2 = df_sta_2.drop(df_sta_2.index[0])
+    df_sta_2 = df_sta_2.drop(df_sta_2.index[0])
+    
+    df_sta_2.index = pd.DatetimeIndex(df_sta_2.index)
+    df_sta_3 = df_sta_2.resample('Y').mean()
+    year=df_sta_3.index.year
+    
+    output_filepath_name=os.path.join(output_filepath,'data.nc')
+    data=np.zeros((len(year),len(gridy),len(gridx)))
+    
+    for i in np.arange(len(year)):
+        value_sta=df_sta_3.iloc[i,:].values
+        data[i,:,:]=station_to_grid(lon_sta,lat_sta,value_sta,gridx,gridy,method,str(year[i]))
+
+    nc_file = nc.Dataset(output_filepath_name, 'w', format='NETCDF4',encoding='gbk')
+    nc_file.createDimension('lon', gridx.shape[0])
+    nc_file.createDimension('lat', gridy.shape[0])
+    nc_file.createDimension('time', year.shape[0])
+
+    lon_var = nc_file.createVariable('longitude', 'f4',('lon',))
+    lon_var[:] = gridx
+
+    lat_var = nc_file.createVariable('latitude', 'f8',('lat',))
+    lat_var[:] = gridy  
+
+    time_var = nc_file.createVariable('time', 'f8',('lat',))
+    time_var[:] = year  
+    
+    grid_var = nc_file.createVariable('data_year', 'f4', ('time','lat', 'lon',))  # grid
+    grid_var[:] = data
+
+    nc_file.close()
+            
+
+    # 变率、最大值、最小值、距平、距平百分率、气候值、与上一年比较值、近10年均值、与近10年比较值
+    ele_choose=['变率','最大值','最小值','距平','距平百分率%','参考时段均值','与上一年比较值','近10年均值','与近10年比较值']
+    
+    i=0
+    for ele in ele_choose:
+        value_sta=df_sta_1[ele].values
+        ele_name='data'+str(i)
+        data=station_to_grid(lon_sta,lat_sta,value_sta,gridx,gridy,method,ele)
+        
+        nc_file = Dataset(output_filepath_name, 'a', format='NETCDF4', encoding='gbk')
+
+        grid_var = nc_file.createVariable(ele_name, 'f4', ('time','lat', 'lon',))  # grid
+        grid_var[:] = data
+
+        nc_file.close()
+        i=i+1
+        
+    result['data']=output_filepath_name
+    
+
+    
+    return result
+
+if __name__ == "__main__":
+    output_filepath=r'D:\Project\1'
+    shp_name=r'D:\Project\3_项目\11_生态监测评估体系建设-气候服务系统\03-边界矢量\03-边界矢量\08-省州界\省界.shp'
+    method='idw2'
+    
+    result=contour_picture(result_df,data_df,shp_name,method,output_filepath)
---
+++ Utils/station_to_grid.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 16:37:59 2024
+
+@author: EDY
+"""
+
+import pandas as pd
+import numpy as np
+from pykrige.ok import OrdinaryKriging
+from pykrige.uk import UniversalKriging
+from scipy.interpolate import griddata 
+import os
+
+def station_to_grid(lon_sta,lat_sta,value_sta,gridx,gridy,method,name):
+    import numpy as np
+
+    lon,lat=np.meshgrid(gridx,gridy)
+
+    if method=='kri':
+    
+        #%% 克里金
+        
+        #　variogram_model是变差函数模型，pykrige提供 linear, power, gaussian, spherical, exponential, hole-effect几种variogram_model可供选择，默认的为linear模型。
+        # 使用不同的variogram_model，预测效果是不一样的，应该针对自己的任务选择合适的variogram_model。    
+        variogram_model='exponential'
+        
+        krig = OrdinaryKriging(lon_sta,lat_sta,value_sta,variogram_model=variogram_model,verbose=False,enable_plotting=False)
+        data, ss3d = krig.execute("grid", gridx, gridy) 
+        
+    elif  method=='ukri':
+
+        #%% 泛克里金
+          
+        variogram_model='exponential'
+        
+        ukrig = UniversalKriging(lon_sta,lat_sta,value_sta,variogram_model=variogram_model,verbose=False,enable_plotting=False)
+        data, ss3d = ukrig.execute("grid", gridx, gridy)
+        
+    elif method=='idw':
+
+        #%% idw
+        
+        def haversine_dist(lon1,lat1,lon2,lat2):
+          lon1,lat1,lon2,lat2 = map(np.radians, (lon1,lat1,lon2,lat2))
+          radius = 6378.135E3 # radius of Earth, unit:m 
+          dlat = lat2 - lat1
+          dlon = lon2 - lon1
+          arg = np.sin(dlat/2) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2) ** 2
+          dist = 2 * radius * np.arcsin(np.sqrt(arg))
+          return dist
+        
+        def interp_IDW(lon_sta, lat_sta, data_sta, lon2D, lat2D):
+            n_sta = len(lon_sta)
+            ny, nx = np.shape(lon2D)
+            data2D = np.zeros((ny, nx))
+            for j in range(ny):
+                for i in range(nx): #遍历二维每一个格点
+                    dist = [] # 格点至所有站点的距离
+                    for s in range(n_sta):
+                        d = haversine_dist(lon_sta[s], lat_sta[s], lon2D[j,i], lat2D[j,i])
+                        d = np.max([1.0, d])  # aviod divide by zero
+                        dist.append(d)
+                    wgt = 1.0 / np.power(dist, 2)
+                    wgt_sum = np.sum(wgt)
+                    arg_sum = np.sum(np.array(wgt) * np.array(data_sta))
+                    data2D[j,i] = arg_sum / wgt_sum
+            return data2D
+        
+        data = interp_IDW(lon_sta, lat_sta, value_sta, lon, lat)
+        
+        #%% windows临时方法，idw更快，呈现效果差
+    elif method=='idw2':
+        import numpy as np
+        from scipy import spatial
+        
+        def idw_interpolation(point_xy, point_z, grid_xy, leafsize, k, p, eps, offset):
+        
+            grid_shape = grid_xy[0].shape
+            grid_flatten = np.reshape(grid_xy, (2, -1)).T
+            tree = spatial.cKDTree(point_xy, leafsize=leafsize)
+            distances, idx = tree.query(grid_flatten, k=k, eps=eps, p=p)
+            distances += offset
+            weights = point_z[idx.ravel()].reshape(idx.shape)
+            grid_z = np.sum(weights / distances, axis=1) / np.sum(1 / distances, axis=1)
+            grid_z = grid_z.reshape(grid_shape)
+            
+            return grid_z
+        
+        point_xy =  np.column_stack((lon_sta, lat_sta))
+        point_z = value_sta
+        
+        grid_xy = (lon,lat)
+        
+        # 使用idw_interpolation函数进行插值
+        leafsize = 600
+        k = 40
+        p = 2
+        eps = 1e-6
+        offset = 1e-9
+        data = idw_interpolation(point_xy, point_z, grid_xy, leafsize, k, p, eps, offset)
+        data=data.astype(float)
+        #%% griddata
+    elif method=='griddata':
+        data_points = np.column_stack([lon_sta,lat_sta])
+
+        data_values = value_sta
+
+        grid_points = np.column_stack([lon.ravel(),lat.ravel()])
+
+
+        # 使用cubic方法进行插值
+        result = griddata(data_points, data_values, grid_points, method='cubic')
+        data = result.reshape(lon.shape)
+        
+    
+    #%% 画图看结果自测使用
+    '''
+    import matplotlib.pyplot as plt
+    import cartopy.crs as ccrs
+    from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+    import matplotlib
+    matplotlib.use('Agg')
+    
+    plt.rcParams['font.sans-serif'] = ['SimHei']  
+    plt.rcParams['axes.unicode_minus'] = False 
+    
+    lon,lat=np.meshgrid(gridx,gridy)
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection=ccrs.PlateCarree())
+    mesh = ax.contourf(lon,lat,data,transform=ccrs.PlateCarree())
+    
+    ax.scatter(lon_sta,lat_sta)
+    
+    for i in np.arange(len(lon_sta)):
+        ax.text(lon_sta[i],lat_sta[i],str(value_sta[i]),fontsize=8)
+    
+    cbar = fig.colorbar(mesh, ax=ax, extend='both', shrink=0.8)#, ticks=np.linspace(levels[0],levels[-1],10))
+    g1=ax.gridlines(draw_labels=True,linewidth=1,color='none',alpha=0.5,linestyle='--',x_inline=False,y_inline=False)#,xlocs=np.arange(105,125+5,5),ylocs=np.arange(32.5,45+2.5,2.5))
+    g1.top_labels=False
+    g1.right_labels=False
+    g1.xformatter=LONGITUDE_FORMATTER
+    g1.yformatter=LATITUDE_FORMATTER
+    g1.rotate_labels=False
+    
+    result_picture = os.path.join(r'D:\Project\1',name+'.png')
+    fig.savefig(result_picture, dpi=200, bbox_inches='tight')
+    plt.cla()
+    '''
+    
+    return data
+
+if __name__ == "__main__":
+    
+    #%% 数据读取与处理
+    from Utils.data_processing import data_processing
+
+    filename=r'D:\Project\3_项目\2_气候评估和气候可行性论证\qhkxxlz\Files\test_data\qh_year.csv'
+    df=pd.read_csv(filename)
+    yearly_df = data_processing(df)
+    
+    # 每个站求平均
+    station_name=yearly_df['Station_Name'].unique()
+    df_station=pd.DataFrame(columns=station_name)
+    for name in station_name:
+        df_station.at[0,name]=round(np.nanmean(yearly_df[yearly_df['Station_Name']==name]['TEM_Max']),2)
+        df_station.at[1,name]=yearly_df[yearly_df['Station_Name']==name]['Lon'].iloc[0]
+        df_station.at[2,name]=yearly_df[yearly_df['Station_Name']==name]['Lat'].iloc[0]
+        
+    df_station=df_station.T
+    df_station.columns=['TEM_Max','Lon','Lat']
+    
+    #%% 插值成网格（克里金、泛克里金、反距离加权）
+    # 站点数据
+    lon_sta=df_station['Lon']
+    lat_sta=df_station['Lat']
+    value_sta=df_station['TEM_Max']
+    
+    # 网格参数设置
+    start_lon = 89.5
+    start_lat = 31
+    end_lon = 103.1
+    end_lat = 39.3
+    resolution = 0.01
+        
+    gridx=np.arange(start_lon,end_lon,resolution)
+    gridy=np.arange(start_lat,end_lat,resolution)
+
+    method='kri'
+    data=station_to_grid(lon_sta,lat_sta,value_sta,gridx,gridy,method)
+
+
+    
+
+    
+
