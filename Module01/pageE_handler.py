c1cf8d99dca28f35440bac249d4d4244beaf6505
daimu <1663457357@qq.com>
2024-12-04 15:35:13 +0800

    增加数据管理-冰冻圈冰川数据

--- Module01/pageE_handler.py
+++ Module01/pageE_handler.py
@@ -25,6 +25,9 @@ import time
 from Module01.wrapped.func13_frs_table_stats import frs_table_stats
 from Module01.wrapped.func14_ice_table_stats import snow_table_stats
 from Utils.data_loader_with_threads import get_database_data
+import psycopg2
+from psycopg2 import sql
+from Module01.wrapped.func01_table_stats import table_stats
 
 # 冰冻圈
 
@@ -43,6 +46,10 @@ def freeze_features_stats(data_json):
        查询统计 - 冰冻圈 - 积雪
        最大积雪深度：SNOW_DEPTH
        积雪日数： SNOW_DAYS
+       
+       查询统计 - 冰冻圈 - 冰川
+       冰川面积：ICE_AREA
+       冰储量： ICE_RESERVES
 
     :param refer_years: 对应原型的参考时段，只和气候值统计有关，传参：'%Y,%Y'
 
@@ -106,88 +113,150 @@ def freeze_features_stats(data_json):
 
     elif element in ['SNOW_DEPTH', 'SNOW_DAYS']:
         element_str = 'snow_depth'
-
-    table_name = 'qh_climate_cmadaas_day'
-    # 从数据库截数据
-    sta_ids = tuple(sta_ids.split(','))
-    elements = 'Station_Id_C,Station_Name,Lon,Lat,Datetime,' + element_str
-
-    data_df = get_database_data(sta_ids, elements, table_name, time_freq, stats_times)
-    refer_df = get_database_data(sta_ids, elements, table_name, time_freq, refer_years)
-    nearly_df = get_database_data(sta_ids, elements, table_name, time_freq, nearly_years)
- 
-    # 统计年份数据处理为df
-    data_df.set_index('Datetime', inplace=True)
-    data_df.index = pd.DatetimeIndex(data_df.index)
-    data_df['Station_Id_C'] = data_df['Station_Id_C'].astype(str)
-
-    if 'Unnamed: 0' in data_df.columns:
-        data_df.drop(['Unnamed: 0'], axis=1, inplace=True)
-
-    refer_df.set_index('Datetime', inplace=True)
-    refer_df.index = pd.DatetimeIndex(refer_df.index)
-    refer_df['Station_Id_C'] = refer_df['Station_Id_C'].astype(str)
-
-    if 'Unnamed: 0' in refer_df.columns:
-        refer_df.drop(['Unnamed: 0'], axis=1, inplace=True)
-
-    # 下载近10年的数据
-    nearly_df.set_index('Datetime', inplace=True)
-    nearly_df.index = pd.DatetimeIndex(nearly_df.index)
-    nearly_df['Station_Id_C'] = nearly_df['Station_Id_C'].astype(str)
-
-    if 'Unnamed: 0' in nearly_df.columns:
-        nearly_df.drop(['Unnamed: 0'], axis=1, inplace=True)
-
-    ###################################################
-    # 开始计算
-    result_dict = dict()
-    result_dict['uuid'] = uuid4
-
-    # 首先获取站号对应的站名
-    station_df = pd.DataFrame()
-    station_df['站号'] = [
-        51886, 51991, 52602, 52633, 52645, 52657, 52707, 52713, 52737, 52745, 52754, 52765, 52818, 52825, 52833, 52836, 52842, 52851, 52853, 52855, 52856, 52859, 52862, 52863, 52866, 52868, 52869, 52874, 52875, 52876, 52877, 52908, 52942, 52943,
-        52955, 52957, 52963, 52968, 52972, 52974, 56004, 56015, 56016, 56018, 56021, 56029, 56033, 56034, 56043, 56045, 56046, 56065, 56067, 56125, 56151
-    ]
-    station_df['站名'] = [
-        '茫崖', '那陵格勒', '冷湖', '托勒', '野牛沟', '祁连', '小灶火', '大柴旦', '德令哈', '天峻', '刚察', '门源', '格尔木', '诺木洪', '乌兰', '都兰', '茶卡', '江西沟', '海晏', '湟源', '共和', '瓦里关', '大通', '互助', '西宁', '贵德', '湟中', '乐都', '平安', '民和', '化隆', '五道梁', '河卡', '兴海', '贵南', '同德', '尖扎', '泽库',
-        '循化', '同仁', '沱沱河', '曲麻河', '治多', '杂多', '曲麻莱', '玉树', '玛多', '清水河', '玛沁', '甘德', '达日', '河南', '久治', '囊谦', '班玛'
-    ]
-    station_df['站号'] = station_df['站号'].map(str)
-    new_station = station_df[station_df['站号'].isin(sta_ids)]
-
-    # stats_result 展示结果表格
-    # post_data_df 统计年份数据，用于后续计算
-    # post_refer_df 参考年份数据，用于后续计算
-
-    if element in ['FRS_DEPTH', 'FRS_TIME']:
-        stats_result, post_data_df, post_refer_df, reg_params = frs_table_stats(data_df, refer_df, nearly_df, element, last_year)
-
-    elif element in ['FRS_START', 'FRS_END']:
-        stats_result, post_data_df, post_refer_df, reg_params, data_df_time = frs_table_stats(data_df, refer_df, nearly_df, element, last_year)
         
-        # 增加时间处理为str
-        for col in data_df_time.columns:
-            data_df_time[col] = data_df_time[col].apply(lambda x: np.nan if pd.isnull(x) else x.strftime('%Y-%m-%d'))
-        data_df_time.dropna(how='all',axis=0,inplace=True)
-        data_df_time.reset_index(drop=False,inplace=True)
+    elif element in ['ICE_AREA']:
+        element_str = 'area'
         
-    elif element in ['SNOW_DEPTH', 'SNOW_DAYS']:
-        stats_result, post_data_df, post_refer_df, reg_params = snow_table_stats(data_df, refer_df, nearly_df, element, time_freq, last_year)
+    elif element in ['ICE_RESERVES']:
+        element_str = 'bcid'
+    
+    if element in ['FRS_DEPTH', 'FRS_START', 'FRS_END', 'FRS_TIME','SNOW_DEPTH', 'SNOW_DAYS']:
+        table_name = 'qh_climate_cmadaas_day'
+        # 从数据库截数据
+        sta_ids = tuple(sta_ids.split(','))
+        elements = 'Station_Id_C,Station_Name,Lon,Lat,Datetime,' + element_str
+    
+        data_df = get_database_data(sta_ids, elements, table_name, time_freq, stats_times)
+        refer_df = get_database_data(sta_ids, elements, table_name, time_freq, refer_years)
+        nearly_df = get_database_data(sta_ids, elements, table_name, time_freq, nearly_years)
+     
+        # 统计年份数据处理为df
+        data_df.set_index('Datetime', inplace=True)
+        data_df.index = pd.DatetimeIndex(data_df.index)
+        data_df['Station_Id_C'] = data_df['Station_Id_C'].astype(str)
+    
+        if 'Unnamed: 0' in data_df.columns:
+            data_df.drop(['Unnamed: 0'], axis=1, inplace=True)
+    
+        refer_df.set_index('Datetime', inplace=True)
+        refer_df.index = pd.DatetimeIndex(refer_df.index)
+        refer_df['Station_Id_C'] = refer_df['Station_Id_C'].astype(str)
+    
+        if 'Unnamed: 0' in refer_df.columns:
+            refer_df.drop(['Unnamed: 0'], axis=1, inplace=True)
+    
+        # 下载近10年的数据
+        nearly_df.set_index('Datetime', inplace=True)
+        nearly_df.index = pd.DatetimeIndex(nearly_df.index)
+        nearly_df['Station_Id_C'] = nearly_df['Station_Id_C'].astype(str)
+    
+        if 'Unnamed: 0' in nearly_df.columns:
+            nearly_df.drop(['Unnamed: 0'], axis=1, inplace=True)
+    
+        ###################################################
+        # 开始计算
+        result_dict = dict()
+        result_dict['uuid'] = uuid4
+    
+        # 首先获取站号对应的站名
+        station_df = pd.DataFrame()
+        station_df['站号'] = [
+            51886, 51991, 52602, 52633, 52645, 52657, 52707, 52713, 52737, 52745, 52754, 52765, 52818, 52825, 52833, 52836, 52842, 52851, 52853, 52855, 52856, 52859, 52862, 52863, 52866, 52868, 52869, 52874, 52875, 52876, 52877, 52908, 52942, 52943,
+            52955, 52957, 52963, 52968, 52972, 52974, 56004, 56015, 56016, 56018, 56021, 56029, 56033, 56034, 56043, 56045, 56046, 56065, 56067, 56125, 56151
+        ]
+        station_df['站名'] = [
+            '茫崖', '那陵格勒', '冷湖', '托勒', '野牛沟', '祁连', '小灶火', '大柴旦', '德令哈', '天峻', '刚察', '门源', '格尔木', '诺木洪', '乌兰', '都兰', '茶卡', '江西沟', '海晏', '湟源', '共和', '瓦里关', '大通', '互助', '西宁', '贵德', '湟中', '乐都', '平安', '民和', '化隆', '五道梁', '河卡', '兴海', '贵南', '同德', '尖扎', '泽库',
+            '循化', '同仁', '沱沱河', '曲麻河', '治多', '杂多', '曲麻莱', '玉树', '玛多', '清水河', '玛沁', '甘德', '达日', '河南', '久治', '囊谦', '班玛'
+        ]
+        station_df['站号'] = station_df['站号'].map(str)
+        new_station = station_df[station_df['站号'].isin(sta_ids)]
+        
+        # stats_result 展示结果表格
+        # post_data_df 统计年份数据，用于后续计算
+        # post_refer_df 参考年份数据，用于后续计算
+        if element in ['FRS_DEPTH', 'FRS_TIME']:
+            stats_result, post_data_df, post_refer_df, reg_params = frs_table_stats(data_df, refer_df, nearly_df, element, last_year)
+
+        elif element in ['FRS_START', 'FRS_END']:
+            stats_result, post_data_df, post_refer_df, reg_params, data_df_time = frs_table_stats(data_df, refer_df, nearly_df, element, last_year)
+            
+            # 增加时间处理为str
+            for col in data_df_time.columns:
+                data_df_time[col] = data_df_time[col].apply(lambda x: np.nan if pd.isnull(x) else x.strftime('%Y-%m-%d'))
+            data_df_time.dropna(how='all',axis=0,inplace=True)
+            data_df_time.reset_index(drop=False,inplace=True)
+            
+        elif element in ['SNOW_DEPTH', 'SNOW_DAYS']:
+            stats_result, post_data_df, post_refer_df, reg_params = snow_table_stats(data_df, refer_df, nearly_df, element, time_freq, last_year)
+        
+        
+    
+    elif element in ['ICE_AREA', 'ICE_RESERVES']:
+
+        def get_bingchuan_data(sta_ids,element_str,stats_times):
+            sta_ids = tuple(sta_ids.split(','))
+
+            conn = psycopg2.connect(database=cfg.INFO.DB_NAME, user=cfg.INFO.DB_USER, password=cfg.INFO.DB_PWD, host=cfg.INFO.DB_HOST, port=cfg.INFO.DB_PORT)
+            cur = conn.cursor()
+            
+            elements = 'datatime,zone,bcname,bcid,' + element_str
+    
+            query = sql.SQL(f"""
+                            SELECT {elements}
+                            FROM public.qh_climate_bingchuan
+                            WHERE
+                                CAST(SUBSTRING(datatime FROM 1 FOR 4) AS INT) BETWEEN %s AND %s
+                                AND bcid IN %s
+                            """)
+    
+            # 根据sql获取统计年份data
+            start_year = stats_times.split(',')[0]
+            end_year = stats_times.split(',')[1]
+            cur.execute(query, (start_year, end_year, sta_ids))
+            data = cur.fetchall()
+    
+            df = pd.DataFrame(data)
+            df.columns = elements.split(',')
+            df[element_str] = df[element_str].astype(float).apply(lambda x: np.nan if x > 9999 else x)
+            
+            df.rename(columns={'bcid': 'Station_Id_C'}, inplace=True)
+            df.rename(columns={'datatime': 'Datetime'}, inplace=True)
+
+            df.set_index('Datetime', inplace=True)
+            df.index =pd.to_datetime(df.index)
+            
+            cur.close()
+            conn.close()
+            
+            return df
+        
+        data_df = get_bingchuan_data(sta_ids, element_str, stats_times)
+        refer_df = get_bingchuan_data(sta_ids, element_str, refer_years)
+        nearly_df = get_bingchuan_data(sta_ids, element_str, nearly_years)
+
+        stats_result, post_data_df, post_refer_df, reg_params = table_stats(data_df, refer_df, nearly_df, element_str, last_year)
+        print('统计表完成')
+
+        new_station=None
+    
+    
     
     # 分布图 try在里面了
-    if shp_path is not None:
-        nc_path, _, _, _, _ = contour_picture(stats_result, data_df, shp_path, interp_method, data_dir)
-        nc_path_trans = nc_path.replace(cfg.INFO.IN_DATA_DIR, cfg.INFO.OUT_DATA_DIR)  # 容器内转容器外路径
-        nc_path_trans = nc_path_trans.replace(cfg.INFO.OUT_DATA_DIR, cfg.INFO.OUT_DATA_URL)  # 容器外路径转url
-        print('分布图插值生成nc完成')
-    else:
+    try:
+        if shp_path is not None:
+            nc_path, _, _, _, _ = contour_picture(stats_result, data_df, shp_path, interp_method, data_dir)
+            nc_path_trans = nc_path.replace(cfg.INFO.IN_DATA_DIR, cfg.INFO.OUT_DATA_DIR)  # 容器内转容器外路径
+            nc_path_trans = nc_path_trans.replace(cfg.INFO.OUT_DATA_DIR, cfg.INFO.OUT_DATA_URL)  # 容器外路径转url
+            print('分布图插值生成nc完成')
+        else:
+            nc_path = None
+            nc_path_trans = None
+    except:
         nc_path = None
         nc_path_trans = None
 
     # 6/7. 统计分析-EOF分析
-    if nc_path is not None:
+    try:
         try:
             ds = xr.open_dataset(nc_path)
             eof_path = eof(ds, shp_path, data_dir)
@@ -197,33 +266,54 @@ def freeze_features_stats(data_json):
             eof_path = None
             reof_path = None
             print('没有插值生成网格文件，无法计算eof/reof')
-
+    except:
+        eof_path = None
+        reof_path = None
     # 测试下来，只有1个值也能出结果，以下所有的暂时不用加异常处理
     # 1.统计分析-mk检验
-    mk_result = time_analysis(post_data_df, data_dir)
-    print('MK检验完成')
-
+    try:
+        mk_result = time_analysis(post_data_df, data_dir)
+        print('MK检验完成')
+    except:
+        mk_result = None
+        
     # 2.统计分析-累积距平
-    anomaly_result = calc_anomaly_cum(post_data_df, post_refer_df, data_dir)
-    print('距平完成')
-
+    try:
+        anomaly_result = calc_anomaly_cum(post_data_df, post_refer_df, data_dir)
+        print('距平完成')
+    except:
+        anomaly_result = None
+        
     # 3.统计分析-滑动平均
-    moving_result = calc_moving_avg(post_data_df, 5, data_dir)
-    print('滑动平均完成')
-
+    try:
+        moving_result = calc_moving_avg(post_data_df, 5, data_dir)
+        print('滑动平均完成')
+    except:
+        moving_result = None
+        
     # 4. 统计分析-小波分析
-    wave_result = wavelet_main(post_data_df, data_dir)
-    print('小波完成')
-
+    try:
+        wave_result = wavelet_main(post_data_df, data_dir)
+        print('小波完成')
+    except:
+        wave_result = None
+        
     # 5. 统计分析-相关分析
-    correlation_result = correlation_analysis(post_data_df, data_dir)
-    print('相关分析完成')
-
+    try:
+        correlation_result = correlation_analysis(post_data_df, data_dir)
+        print('相关分析完成')
+    except:
+        correlation_result = None
+        
     # 8.EEMD分析
-    eemd_result = eemd(post_data_df, data_dir)
-    print('eemd完成')
-
+    try:
+        eemd_result = eemd(post_data_df, data_dir)
+        print('eemd完成')
+    except:
+        eemd_result = None
+        
     # 数据保存
+    result_dict=dict()
     result_dict['表格'] = dict()
     result_dict['表格'] = stats_result.to_dict(orient='records')
 
@@ -242,7 +332,11 @@ def freeze_features_stats(data_json):
     result_dict['统计分析']['EOF分析'] = eof_path
     result_dict['统计分析']['REOF分析'] = reof_path
     result_dict['统计分析']['EEMD分析'] = eemd_result
-    result_dict['站号'] = new_station.to_dict(orient='records')
+    try:
+        result_dict['站号'] = new_station.to_dict(orient='records')
+    except:
+        result_dict['站号'] = new_station
+
 
     return result_dict
 
@@ -250,12 +344,12 @@ def freeze_features_stats(data_json):
 if __name__ == '__main__':
     t1 = time.time()
     data_json = dict()
-    data_json['element'] = 'SNOW_DEPTH'
-    data_json['refer_years'] = '1990,2020'
-    data_json['nearly_years'] = '2002,2022'
+    data_json['element'] = 'ICE_AREA'
+    data_json['refer_years'] = '2003,2018'
+    data_json['nearly_years'] = '2003,2010'
     data_json['time_freq'] = 'Y'
     data_json['stats_times'] = '2010,2019'  # '198105,202009' # '1981,2023'
-    data_json['sta_ids'] = '52908,52943,52955,52957,52968,56004,56016'
+    data_json['sta_ids'] = '3020201'
     data_json['interp_method'] = 'idw'
     data_json['ci'] = 95
     data_json['shp_path'] = r'D:\Project\3_项目\11_生态监测评估体系建设-气候服务系统\材料\03-边界矢量\03-边界矢量\01-青海省\青海省县级数据.shp'
