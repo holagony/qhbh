0265ad274465167a13c3f0457cffcaff20d0a751
majunyan <mjynj1993@163.com>
2024-12-11 13:43:23 +0800

    水资源接口更新

--- Module02/module02_flask.py
+++ Module02/module02_flask.py
@@ -6,8 +6,12 @@ from Module02.page_energy.page_energe_heating_handler import energy_winter_heati
 from Module02.page_energy.page_energe_wind_handler import energy_wind_power
 from Module02.page_energy.page_energe_solar_handler import energy_solar_power
 from Module02.page_ice.page_ice_model_handler import ice_model_def
-from Module02.page_water.page_water_hbv_handler import hbv_single_calc
 from Module02.page_ice.page_ice_table_handler import ice_table_def
+
+from Module02.page_water.page_water_hbv_handler import hbv_single_calc
+from Module02.page_water.page_water_model_handler import water_model_def
+from Module02.page_water.page_water_table_handler import water_table_def
+
 from Module02.page_climate.page_climate_handler import climate_esti
 from Module02.page_traffic.traffic_handler import traffic_esti
 from Module02.page_grass.page_grass_model_handler import grass_model_def
@@ -113,6 +117,38 @@ def pageb_water_hbv():
         return jsonify({'code': 202, 'msg': '任务提交成功，开始计算...', 'data': {'task_id': result.id}})
 
     result_dict, _ = hbv_single_calc(data_json)
+    return_data = simplejson.dumps({'code': 200, 'msg': 'success', 'data': result_dict}, ensure_ascii=False, ignore_nan=True)
+    return return_data
+
+@module02.route('/v1/water_model', methods=['POST'])
+def pageb_water_hbv():
+    '''
+    重点领域与行业预估-水资源影响预估-统计建模
+    '''
+    json_str = request.get_data(as_text=True)  # 获取JSON字符串
+    data_json = json.loads(json_str)
+    is_async = data_json.get('is_async')
+    if is_async == 1 or is_async is True or is_async == '1':
+        result = celery_submit.delay('workerPageWaterModel', json_str)
+        return jsonify({'code': 202, 'msg': '任务提交成功，开始计算...', 'data': {'task_id': result.id}})
+
+    result_dict = water_model_def(data_json)
+    return_data = simplejson.dumps({'code': 200, 'msg': 'success', 'data': result_dict}, ensure_ascii=False, ignore_nan=True)
+    return return_data
+
+@module02.route('/v1/water_source_hbv', methods=['POST'])
+def pageb_water_hbv():
+    '''
+    重点领域与行业预估-水资源影响预估-HBV
+    '''
+    json_str = request.get_data(as_text=True)  # 获取JSON字符串
+    data_json = json.loads(json_str)
+    is_async = data_json.get('is_async')
+    if is_async == 1 or is_async is True or is_async == '1':
+        result = celery_submit.delay('workerPageWaterTable', json_str)
+        return jsonify({'code': 202, 'msg': '任务提交成功，开始计算...', 'data': {'task_id': result.id}})
+
+    result_dict = water_table_def(data_json)
     return_data = simplejson.dumps({'code': 200, 'msg': 'success', 'data': result_dict}, ensure_ascii=False, ignore_nan=True)
     return return_data
 
